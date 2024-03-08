b67dc8719212554b073fd99182ab41ccbaabb672
Amir <a.presovsky@list.ru>
2024-03-08 18:22:26 +0300

    deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom/client'
 import Router from './Components/Router/Router.tsx'
 import './index.css'
+import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <Router />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  </React.StrictMode>
 )
